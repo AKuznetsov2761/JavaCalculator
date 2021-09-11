b474dc318ed810b40a6810d6466fd83c88ef25f8
AKuznetsov2761 <akuznetsov2761@gmail.com>
2021-09-11 18:36:17 +0300

    Create Main.java
    Основной выполняемый блок

---
+++ src/akuznetsov/javacalc/Main.java
@@ -0,0 +1,15 @@
+package akuznetsov.javacalc;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Тест");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
+
+
