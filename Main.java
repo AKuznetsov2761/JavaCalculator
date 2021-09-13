b10aed427eb53b8e0f144abf781cba28e7168608
AKuznetsov2761 <90379167+AKuznetsov2761@users.noreply.github.com>
2021-09-13 21:42:04 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            System.out.println("Enter 2 integers from 1 to 10 (roman or arabic) and the only 1 operator: + - * /  Samples: Х - IV, 10 * 9 etc.");
+            System.out.println("Enter 2 integers from 1 to 10 (roman or arabic) and the only 1 operator: + - * / , examples: X - IV, 10 * 9 etc.");
             String instring = new Scanner(System.in).nextLine();
             // System.out.println("Вы ввели: " + instring);
             StringSplitter.split(instring);
